6a5519ba62277117d50906606bde8cbad4794cd8
Me <me@test.com>
2026-02-04 09:37:56 +0100

    Fix config.js Mobile App

--- excellia-mobile/src/config.js
+++ excellia-mobile/src/config.js
@@ -2,6 +2,6 @@
 // Android emulator: http://10.0.2.2:5000/api
 // iOS simulator: http://localhost:5000/api
 // Real phone: http://YOUR_PC_IP:5000/api
-export const API_BASE_URL = 'http://10.1.0.44:5000/api';
+export const API_BASE_URL ='https://excellia-checkwork-849z.onrender.com/api';
 
 export const APP_NAME = 'Excellia Mobile';
