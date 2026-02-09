e16a692a31dacb7afe410330b4e197bfaa8e0634
Me <me@test.com>
2026-02-09 12:18:16 +0100

    Add Docker deployment config

--- excellia-mobile/src/config.js
+++ excellia-mobile/src/config.js
@@ -2,6 +2,6 @@
 // Android emulator: http://10.0.2.2:5000/api
 // iOS simulator: http://localhost:5000/api
 // Real phone: http://YOUR_PC_IP:5000/api
-export const API_BASE_URL ='https://excellia-checkwork-849z.onrender.com/api';
+export const API_BASE_URL ='https://https://172.26.10.81/api';
 
 export const APP_NAME = 'Excellia Mobile';
