e1c6bdfe364e07d6521595dd8731da6393bbf018
Me <me@test.com>
2026-02-10 15:11:03 +0100

    Fixed mobile connection

--- excellia-mobile/src/config.js
+++ excellia-mobile/src/config.js
@@ -2,6 +2,6 @@
 // Android emulator: http://10.0.2.2:5000/api
 // iOS simulator: http://localhost:5000/api
 // Real phone: http://YOUR_PC_IP:5000/api
-export const API_BASE_URL ='https://https://172.26.10.81/api';
+export const API_BASE_URL ='http://197.5.144.190/api';
 
 export const APP_NAME = 'Excellia Mobile';
