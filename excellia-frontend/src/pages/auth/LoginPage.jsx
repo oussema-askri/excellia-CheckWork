62a6a81578ea9b7b6a458de6d8487d066fa2a78f
Me <me@test.com>
2026-02-26 10:16:28 +0100

    Fix Logo V3

--- excellia-frontend/src/pages/auth/LoginPage.jsx
+++ excellia-frontend/src/pages/auth/LoginPage.jsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
   const { darkMode, toggleDarkMode } = useTheme()
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-primary-600 to-primary-800 dark:from-gray-900 dark:to-gray-800 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg:white from-primary-600 to-primary-800 dark:from-gray-900 dark:to-gray-800 py-12 px-4 sm:px-6 lg:px-8">
       {/* Dark Mode Toggle */}
       <button
         onClick={toggleDarkMode}
